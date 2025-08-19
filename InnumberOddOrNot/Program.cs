a07a92d4fc49bbb6a631f35233984658125e9c42
jaarojas <jaarojas@MEDSISCGEGOD801>
2025-08-19 13:29:59 -0500

    Add project files.

---
+++ InnumberOddOrNot/Program.cs
@@ -0,0 +1,28 @@
+﻿var numberString = string.Empty;
+do
+{
+    Console.Write("Escribe un número entero o 'salir' para salir ");
+    numberString = Console.ReadLine();
+    if(numberString!.ToLower() == "salir")
+    {
+        continue;
+    }
+    var numberInt = 0;
+    if (int.TryParse(numberString, out numberInt))
+    {
+        if (numberInt % 2 == 0)
+        {
+            Console.WriteLine($"{numberInt} es par");
+        }
+        else
+        {
+            Console.WriteLine($"{numberInt} es impar");
+        }
+
+    }
+    else
+    {
+        Console.WriteLine($"{numberString} no es un número entero");
+    }
+}
+while (numberString.ToLower() != "salir");
