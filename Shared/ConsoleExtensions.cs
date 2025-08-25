7630617133c9a5265822d31dbdeba8bf1c6a881e
jarlenandres <jaarojas@gmai.com>
2025-08-25 15:43:48 -0500

    Code update

--- Shared/ConsoleExtensions.cs
+++ Shared/ConsoleExtensions.cs
@@ -4,7 +4,17 @@
     {
         public static int GetInt(string message)
         {
-            Console.Write("");
+            Console.Write(message);
+            var input = Console.ReadLine();
+            if (int.TryParse(input, out var result))
+            {
+                return result;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+                return GetInt(message);
+            }
         }
     }
 }
