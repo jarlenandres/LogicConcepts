b4f11e0eeccf2a2bd20afe2a0f98901d919fc47a
jarlenandres <jaarojas@gmai.com>
2025-10-24 09:43:09 -0500

    Update merchandise code

--- Mercharndise/Program.cs
+++ Mercharndise/Program.cs
@@ -3,7 +3,7 @@ Console.WriteLine("Shipping of Mercharndise");
 Console.WriteLine();
 
 var answer = string.Empty;
-var isMonday = string.Empty;
+string isMonday = string.Empty;
 var type = string.Empty;
 var pay = new List<string> { "e", "t" };
 var options = new List<string> { "s", "n" };
@@ -11,24 +11,29 @@ do
 {
     var weight = ConsoleExtension.GetInt("Peso de la mercancía: ");
     var value = ConsoleExtension.GetDecimal("Valor de la mercancía: ");
-    isMonday = ConsoleExtension.GetValidOptions("¿Es lunes? [S]í [N]o: ", options);
-    type = ConsoleExtension.GetValidOptions("Tipo de pago [E]fectivo [T]arjeta: ", pay);
+
+    do
+    {
+        isMonday = ConsoleExtension.GetValidOptions("¿Es lunes? [S]í [N]o: ", options)!;
+    } while (!options.Any(x => x.Equals(isMonday, StringComparison.OrdinalIgnoreCase)));
+    
+    do
+    {
+        type = ConsoleExtension.GetValidOptions("Tipo de pago [E]fectivo [T]arjeta: ", pay)!;
+    } while (!pay.Any(x => x.Equals(type, StringComparison.OrdinalIgnoreCase)));
 
     var fee = CalculateFee(weight);
-    var discount = CalculateDiscount(value, fee);
-    var total = CalculateTotal(fee, isMonday!, type!, value, discount);
-
-    Console.WriteLine($"Tarifa: {fee}");
-    if (isMonday == "s" && type == "t")
+    var discount = CalculateDiscount(value, fee, isMonday!, type!);
+    Console.WriteLine($"Tarifa....: {fee}");
+    if (isMonday.ToLower() == "s" && type.ToLower() == "t")
     {
-        Console.WriteLine($"Promoción: {discount}");
-
+        Console.WriteLine($"Promoción....: {discount}");
     }
     else
     {
-        Console.WriteLine($"Descuento: {discount}");
+        Console.WriteLine($"Descuento....: {discount}");
     }
-    Console.WriteLine($"Total a pagar: {total}");
+    Console.WriteLine($"Total a pagar....: {fee - discount}");
     do
         {
             answer = ConsoleExtension.GetValidOptions("¿Desea continuar? (S/N): ", options);
@@ -58,40 +63,29 @@ decimal CalculateFee(int weight)
     return 0m;
 }
 
-decimal CalculateDiscount(decimal value, decimal fee)
+decimal CalculateDiscount(decimal value, decimal fee, string isMonday, string type)
 {
     decimal discount;
-    if (value >= 300000 && value <= 600000)
+    if (value >= 300000 && value <= 600000 && isMonday.ToLower() != "s" && type.ToLower() != "t")
     {
         return discount = fee * 0.10m;
     }
-    if (value > 600000 && value <= 1000000)
+    if (value > 600000 && value <= 1000000 && isMonday.ToLower() != "s" && type.ToLower() != "t")
     {
         return discount = fee * 0.20m;
     }
-    if (value > 1000000)
+    if (isMonday.ToLower() == "s" && type.ToLower() == "t")
+    {
+        return discount = fee / 2m;
+    }
+    if (type.ToLower() == "e" && value > 1000000)
+    {
+        return discount = fee * 0.40m;
+    }
+    if (value > 1000000 && type.ToLower() != "e")
     {
         return discount = fee * 0.30m;
     }
     return 0m;
 }
 
-decimal CalculateTotal(decimal fee, string isMonday, string type, decimal value, decimal discount)
-{
-    decimal total = 0m;
-    if (isMonday == "s" && type == "t")
-    {
-        total = fee * 0.50m;
-        return total;
-    }
-    if (type == "e" && value > 1000000)
-    {
-        total = fee * 0.40m;
-        return total;
-    }
-    else
-    {
-        total = fee - discount;
-        return total;
-    }
-}
